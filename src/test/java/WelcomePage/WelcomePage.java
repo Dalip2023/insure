2e8ff6414ca496c150f734a031c9d34045a3e560
dalip <sdet.dalip@gmail.com>
2023-02-07 17:04:10 +0530

    added verifyNeedHelpLink method

--- src/test/java/WelcomePage/WelcomePage.java
+++ src/test/java/WelcomePage/WelcomePage.java
@@ -13,8 +13,8 @@ import org.testng.annotations.Test;
 
 import CommonLib.CommonLib;
 
-public class WelcomePage extends CommonLib{
-	
+public class WelcomePage extends CommonLib {
+
 	@BeforeClass
 	public static void open_browser() throws InterruptedException, MalformedURLException {
 		CommonLib.LaunchBrowser();
@@ -25,68 +25,71 @@ public class WelcomePage extends CommonLib{
 		CommonLib.closeBrowser();
 		CommonLib.quitDriver();
 	}
-	
+
 	@Test
 	public static void welcomePageTitleCheck() {
-		
+
 		driver.get("https://d3j8nuwp74eyml.cloudfront.net/5U5PU/S2xbn/UGFnZV8w");
-		
+
 		WebDriverWait Welcome_page = new WebDriverWait(driver, 30);
-		Welcome_page.until(ExpectedConditions
-				.visibilityOfElementLocated(By.xpath("//button[@type='submit']")));
-		
+		Welcome_page.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit']")));
+
 		String welcomePageTitleActual = driver.getTitle();
-		
+
 		String welcomePageTitleExpected = "Welcome";
-		
+
 		Assert.assertEquals(welcomePageTitleActual, welcomePageTitleExpected);
-		
-		
+
 	}
-	
+
 	@Test
-    public static void welcomePageLogoCheck() {
-	
-	
-	driver.get("https://d3j8nuwp74eyml.cloudfront.net/5U5PU/S2xbn/UGFnZV8w");
-	
-	WebDriverWait Welcome_page = new WebDriverWait(driver, 30);
-	Welcome_page.until(ExpectedConditions
-			.visibilityOfElementLocated(By.xpath("//img[@alt='Header Logo']")));
-	
-	WebElement logo = driver.findElement(By.xpath("//img[@alt='Header Logo']"));
-	
-	Boolean logoPresent = logo.isDisplayed();
-	
-	System.out.println(logoPresent);
-	
-		
+	public static void welcomePageLogoCheck() {
+
+		driver.get("https://d3j8nuwp74eyml.cloudfront.net/5U5PU/S2xbn/UGFnZV8w");
+
+		WebDriverWait Welcome_page = new WebDriverWait(driver, 30);
+		Welcome_page.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[@alt='Header Logo']")));
+
+		WebElement logo = driver.findElement(By.xpath("//img[@alt='Header Logo']"));
+
+		Boolean logoPresent = logo.isDisplayed();
+
+		System.out.println(logoPresent);
+
 	}
-	
+
+	public static void VerifyNeedHelpLink() {
+
+		driver.get("https://d3j8nuwp74eyml.cloudfront.net/5U5PU/S2xbn/UGFnZV8w");
+
+		WebDriverWait Welcome_page = new WebDriverWait(driver, 30);
+		Welcome_page.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit']")));
+
+		String NeedHelpTextActual = driver.findElement(By.xpath("//div[contains(text(),'Need help?')]")).getText();
+
+		String NeedHelpTextExpected = "Need help";
+
+		Assert.assertEquals(NeedHelpTextActual, NeedHelpTextExpected);
+
+	}
+
 	@Test
-    public static void welcomePageTextShowing() {
-	
-	
-	driver.get("https://d3j8nuwp74eyml.cloudfront.net/5U5PU/S2xbn/UGFnZV8w");
-	
-	WebDriverWait Welcome_page = new WebDriverWait(driver, 30);
-	Welcome_page.until(ExpectedConditions
-			.visibilityOfElementLocated(By.xpath("//button[@type='submit']")));
-	
-	
-	
-	String textwelcomeActual = driver.findElement(By.xpath("//div[@class='text-2xl text-neutral-100 text-center font-medium mb-4']")).getText();
-	System.out.println(textwelcomeActual);
-	
-	String textwelcomeExpected = "?Welcome";
-	
-	Assert.assertEquals(textwelcomeActual, textwelcomeExpected);
-	
+	public static void welcomePageTextShowing() {
+
+		driver.get("https://d3j8nuwp74eyml.cloudfront.net/5U5PU/S2xbn/UGFnZV8w");
+
+		WebDriverWait Welcome_page = new WebDriverWait(driver, 30);
+		Welcome_page.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@type='submit']")));
+
+		String textwelcomeActual = driver
+				.findElement(By.xpath("//div[@class='text-2xl text-neutral-100 text-center font-medium mb-4']"))
+				.getText();
+		System.out.println(textwelcomeActual);
+
+		String textwelcomeExpected = "?Welcome";
+
+		Assert.assertEquals(textwelcomeActual, textwelcomeExpected);
+
 	}
-	
-	
-	
-	
-	
 
 }
